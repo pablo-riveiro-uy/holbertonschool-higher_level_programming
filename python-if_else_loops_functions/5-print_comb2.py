36b064223395ce7faa71dc784398cb870db0c92a
root <priveiro@gmail.com>
2023-05-29 12:10:14 -0700

    stringiformateando

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for n in range(0, 100):
     if n < 10:
-        print('0{}, '.format(n), end='')
+        n = f"0{n}"
+    if n == 99:
+        print(n)
     else:
-            if n != 99:
-                print('{}, '.format(n), end='')
-            else:
-                print(n)
+        print('{}, '.format(n), end='')
+
