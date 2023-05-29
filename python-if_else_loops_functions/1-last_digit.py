10ae3747e3fc22850c605405ec5093abcfc44ad5
root <priveiro@gmail.com>
2023-05-29 10:23:26 -0700

    negative case

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,7 @@ if number > 0:
 else:
     postive_number = number * -1
     last_digit = postive_number % 10
+    last_digit *= -1
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
