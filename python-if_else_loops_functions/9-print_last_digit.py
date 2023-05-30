8d6b421dc33a347a49f888cf3c2a78ba40da21ef
root <priveiro@gmail.com>
2023-05-30 10:43:52 -0700

    chaches

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,7 @@
 def print_last_digit(number):
-    if number < 0:
-        last =  number *-1
-        last = last % 10
-        print("{}".format(last), end="")
-        return last
-    last = number % 10
+    last = number
+    if last < 0:
+        last =  last *-1
+    last = last % 10
     print("{}".format(last), end="")
     return last
