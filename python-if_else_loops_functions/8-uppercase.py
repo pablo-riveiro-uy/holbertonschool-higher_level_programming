1dcf5998b2e539bd2091b5529c000e9175d06c6c
Pablo <priveiro@gmail.com>
2023-05-29 19:50:14 -0700

    task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
+            character = ord(str[i]) - 32
+        else:
+            character = ord(str[i])
+        print("{}".format(chr(character)), end='')
+    print()
