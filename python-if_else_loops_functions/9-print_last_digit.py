07bc4d82231fc002868b58674ffc46e6c8ad3930
root <priveiro@gmail.com>
2023-05-30 10:49:14 -0700

    pycode

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     last = number
     if last < 0:
-        last =  last *-1
+        last = last * -1
     last = last % 10
     print("{}".format(last), end="")
     return last
