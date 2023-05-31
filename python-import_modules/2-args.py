25d3d83cbc1c7ed97344def3d5ac2aae96796086
root <priveiro@gmail.com>
2023-05-31 10:03:29 -0700

    two points thing

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(sys.argv) == 2:
         print("{} argument:".format(len(sys.argv) - 1))
-        print("1 {}".format(sys.argv[1]))
+        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
-            print("{} {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, sys.argv[i]))
