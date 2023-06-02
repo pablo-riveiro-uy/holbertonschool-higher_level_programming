3a01c5d59d16114b34b42723892dede2e6c7f074
TheWhiteMachine <priveiro@gmail.com>
2023-06-02 09:25:34 -0700

    diferent code

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,10 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    just_names = []
     for i in names:
         if i[0] != '_' and i[1] != '_':
-            just_names.append(i)
-    just_names.sort()
-    for i in just_names:
-        print(i)
+            print("{}".format(i))
