131f7f4f77fd928288b9f6a209ba9af241d093e2
Pablo <priveiro@gmail.com>
2023-06-05 15:09:07 -0700

    chanche in 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,12 +2,8 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    elif idx > len(my_list) - 1:
         return my_list
-    other_list = []
-    for i in range(0, my_list[idx] - 1):
-        other_list.append(my_list[i])
-    other_list.append(element)
-    for i in range(my_list[idx], len(my_list)):
-        other_list.append(my_list[i])
-    return other_list
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
