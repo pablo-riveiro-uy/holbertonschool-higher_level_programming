335fe25dfd71856dc158d8352b0ccd75b9cd656b
Pablo <priveiro@gmail.com>
2023-06-05 14:31:31 -0700

    trying not to cast

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,14 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     try:
         matrix[1]
-        c = 0
-        for i in matrix:
-            for j in i:
-                c += 1
-                if (c != 3):
-                    print('{} '.format(j), end="")
-                else:
-                    c = 0
-                    print('{}'.format(j))
+        for i in range(len(matrix)):
+            for j in range(0, len(matrix[i]) - 1):
+                print("{} ".format(matrix[i][j]), end='')
+            print("{}".format(matrix[i][j] + 1))
     except IndexError:
         print()
