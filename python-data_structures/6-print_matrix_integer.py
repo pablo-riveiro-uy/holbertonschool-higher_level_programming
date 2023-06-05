a0b8093a705d462781d6040650b03996643c94eb
Pablo <priveiro@gmail.com>
2023-06-05 15:02:28 -0700

    formatting

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         matrix[1]
         for i in range(len(matrix)):
             for j in range(0, len(matrix[i]) - 1):
-                print("{} ".format(matrix[i][j]), end='')
-            print("{}".format(matrix[i][j] + 1))
+                print("{:d} ".format(matrix[i][j]), end='')
+            print("{:d}".format(matrix[i][j] + 1))
     except IndexError:
         print()
