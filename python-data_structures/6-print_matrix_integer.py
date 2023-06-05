456e977ca91e128b3556fce81d88c36d1a9d78ca
Pablo <priveiro@gmail.com>
2023-06-05 13:13:13 -0700

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            print("{}".format(j), end=" ")
+    try:
+        matrix[1]
+        c = 0
+        for i in matrix:
+            for j in i:
+                c += 1
+                if (c != 3):
+                    print('{} '.format(j), end="")
+                else:
+                    c = 0
+                    print('{}'.format(j))
+    except IndexError:
         print()
