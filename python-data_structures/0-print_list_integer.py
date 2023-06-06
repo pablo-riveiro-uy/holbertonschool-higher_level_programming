a506f1555e62f4d779bed3af1a23f23ce6aa63e8
Pablo <priveiro@gmail.com>
2023-06-06 10:13:31 -0700

    fixing 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     if my_list:
-        for i in my_list:
-            print("{:d}".format(i))
+        i = 0
+        while my_list[i]:
+            print("{:d}".format(my_list[i]))
+            i += 1
