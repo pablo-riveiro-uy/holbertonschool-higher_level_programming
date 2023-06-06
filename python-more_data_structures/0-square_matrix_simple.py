c7b3a281e63de055464cb344e7a2d6456f43ecef
TheWhiteMachine <priveiro@gmail.com>
2023-06-06 14:40:19 -0700

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,9 @@
 def square_matrix_simple(matrix=[]):
     new_list = []
 
-
-    new_list = list(map(*=, matrix))
+    for i in matrix:
+        sub_matrix = []
+        for j in i:
+            sub_matrix.append(j**2)
+        new_list.append(sub_matrix)
     return new_list
