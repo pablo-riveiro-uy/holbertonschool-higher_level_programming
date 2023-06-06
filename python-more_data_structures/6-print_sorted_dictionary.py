6ae39352013af6c9df0d8c5d8011e9c9310914e9
TheWhiteMachine <priveiro@gmail.com>
2023-06-06 16:49:22 -0700

    task 6

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary:
+        sorted_data = sorted(a_dictionary.keys())
+        for x in sorted_data:
+            print("{} : {}".format(x, a_dictionary.get(x)))
