7de04c64b9b3d5e1d2ce759d5d0e6c88f4d45875
root <priveiro@gmail.com>
2023-06-07 10:13:14 -0700

    pycode 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string and type(roman_string) == str:
+    if roman_string and isinstance(roman_string, str):
         nums = []
-        myDict = {"X": 10, "V": 5, "I": 1, "L": 50, "D": 500, "C": 100, "M": 1000}
+        myDict = {"X": 10, "V": 5, "I": 1, "L": 50,
+                  "D": 500, "C": 100, "M": 1000}
         for c in roman_string:
             for s in myDict:
                 if s == c:
