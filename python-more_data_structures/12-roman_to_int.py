6b192db9ceaddce9b7d790e6cc26d8c00e508c7b
root <priveiro@gmail.com>
2023-06-07 08:55:32 -0700

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,16 @@ def roman_to_int(roman_string):
     if roman_string and type(roman_string) == str:
         nums = []
         myDict = {"X": 10, "V": 5, "I": 1, "L": 50, "D": 500, "C": 100}
-        for l in roman_string:
+        for c in roman_string:
             for s in myDict:
-                if s == l:
+                if s == c:
                     nums.append(myDict.get(s))
-        return nums
-
+        added = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i - 1] < nums[i]:
+                added += nums[i] - nums[i - 1] * 2
+            else:
+                added += nums[i]
+        return added
     else:
         return None
