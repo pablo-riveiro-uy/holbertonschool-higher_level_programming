555fb5f2fa9b6585c4e242cda354bc548d7cedb3
TheWhiteMachine <priveiro@gmail.com>
2023-06-06 17:10:31 -0700

    task 7 stoping null

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({str(key): value})
+    if a_dictionary and key and value:
+        a_dictionary.update({str(key): value})
