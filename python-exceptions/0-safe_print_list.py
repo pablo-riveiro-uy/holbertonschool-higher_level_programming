3a5ea78147892828cf296926e6a3a6d10393e47d
TheWhiteMachine <priveiro@gmail.com>
2023-06-07 16:31:40 -0700

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    
+    try:
+        index = x
+    except KeyError:
+        return my_list[i]
+    for i in my_list[0:index]:
+        print("{}".format(i), end="")
+    print()
+    return my_list[i - 1]
