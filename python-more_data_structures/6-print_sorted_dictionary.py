c4ee67449f22c6777423c0e4d4182134c493930d
root <priveiro@gmail.com>
2023-06-07 09:19:16 -0700

    6 fix

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     if a_dictionary:
         sorted_data = sorted(a_dictionary.keys())
         for x in sorted_data:
-            print("{} : {}".format(x, a_dictionary.get(x)))
+            print("{}: {}".format(x, a_dictionary.get(x)))
