b340f9d6acf86800d1bdf2be4e8a8229d2040912
TheWhiteMachine <priveiro@gmail.com>
2023-06-06 17:26:39 -0700

    task 8

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary and key:
+        if key is a_dictionary:
+            a_dictionary.pop(key)
+
+    return a_dictionary
