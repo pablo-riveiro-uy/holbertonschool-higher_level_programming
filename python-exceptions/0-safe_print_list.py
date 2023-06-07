19405d0dc5e16fd55aff67f6573c3df2459b08ac
TheWhiteMachine <priveiro@gmail.com>
2023-06-07 16:39:02 -0700

    checks on 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        index = x
-    except KeyError:
-        return my_list[i]
-    for i in my_list[0:index]:
-        print("{}".format(i), end="")
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+        except Exception:
+            print()
+            return i
     print()
-    return my_list[i - 1]
+    return x
