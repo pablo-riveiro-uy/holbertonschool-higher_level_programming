12b323ea01f9c6827b97940a4fc187f06f252a74
root <priveiro@gmail.com>
2023-06-07 09:52:56 -0700

    fix on 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if a_dictionary and key:
-        if key is a_dictionary:
+        if key in a_dictionary:
             a_dictionary.pop(key)
     return a_dictionary
