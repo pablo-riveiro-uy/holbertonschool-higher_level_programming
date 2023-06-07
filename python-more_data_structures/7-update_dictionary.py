7ca63ed2ac75356c1a8faaf8bfe32909fd0dc461
TheWhiteMachine <priveiro@gmail.com>
2023-06-06 17:18:00 -0700

    adding nono on none

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,4 +2,6 @@
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary and key and value:
         a_dictionary.update({key: value})
+    else:
+        return None
     return a_dictionary
