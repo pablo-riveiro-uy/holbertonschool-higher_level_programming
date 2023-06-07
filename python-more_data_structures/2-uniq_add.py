def8ab07c4b52b256e8e3ac9607ab251de44d29a
root <priveiro@gmail.com>
2023-06-07 10:55:14 -0700

    change 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        add = 0
-        set_nums = set(my_list)
-        for i in set_nums:
-            add += i
-        return add
+    add = 0
+    set_nums = set(my_list)
+    for i in set_nums:
+        add += i
+    return add
