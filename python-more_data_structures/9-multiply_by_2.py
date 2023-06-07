91d6868aac61642b870955f1506d8538d1b84269
TheWhiteMachine <priveiro@gmail.com>
2023-06-06 17:55:02 -0700

    fighting checker round 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -6,5 +6,7 @@ def multiply_by_2(a_dictionary):
             new_value = a_dictionary[i] * 2
             new_dict.update({i: new_value})
         return new_dict
+    elif len(a_dictionary >= 0):
+        return None
     else:
         return None
