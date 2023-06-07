f03a02764732eecbb13e5a5288dd5841e3d3840e
root <priveiro@gmail.com>
2023-06-07 10:05:48 -0700

    adding M to 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if roman_string and type(roman_string) == str:
         nums = []
-        myDict = {"X": 10, "V": 5, "I": 1, "L": 50, "D": 500, "C": 100}
+        myDict = {"X": 10, "V": 5, "I": 1, "L": 50, "D": 500, "C": 100, "M": 1000}
         for c in roman_string:
             for s in myDict:
                 if s == c:
