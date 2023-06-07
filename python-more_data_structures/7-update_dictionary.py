d29928cd514d030db9f35120d55d37c0c05f4b83
root <priveiro@gmail.com>
2023-06-07 09:56:05 -0700

    fix on 7 on null value

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
+    if a_dictionary and value:
         a_dictionary.update({key: value})
     return a_dictionary
