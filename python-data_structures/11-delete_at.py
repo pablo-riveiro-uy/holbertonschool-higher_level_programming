3a98b47cdff9122891f1a1e33afa54f28b908ef0
root <priveiro@gmail.com>
2023-06-08 11:43:23 -0700

    fix on 1 data str 2

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,5 +3,5 @@ def delete_at(my_list=[], idx=0):
     if my_list and idx:
         if idx < 0 or idx > len(my_list) -1:
             return my_list
-        my_list.remove(idx + 1)
+        my_list.remove(my_list[idx])
         return my_list
