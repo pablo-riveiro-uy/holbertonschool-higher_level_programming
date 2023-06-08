55b3631950ab9460fafa7ada0b9f3218e9f7576f
root <priveiro@gmail.com>
2023-06-08 09:18:19 -0700

    checks on 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     printed = 0
     for i in range(0, x):
         try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                printed += 1
-        except Exception:
-            print()
-            return printed
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+        except (TypeError, ValueError):
+            pass
     print()
     return printed
