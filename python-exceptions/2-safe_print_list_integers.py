fbd8e65067efccd5606372824d5a077737545716
root <priveiro@gmail.com>
2023-06-08 07:54:35 -0700

    task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed = 0
+    for i in range(0, x):
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                printed += 1
+        except Exception:
+            print()
+            return printed
+    print()
+    return printed
