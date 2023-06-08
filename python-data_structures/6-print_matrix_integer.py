af033b5d58b928d99f22406260258bd339baaf72
root <priveiro@gmail.com>
2023-06-08 11:33:29 -0700

    fix task 6 data str

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        try:
-            matrix[1]
-            for i in range(len(matrix)):
-                for j in range(0, len(matrix[i]) - 1):
-                    print("{:d}".format(matrix[i][j]), end=' ')
-                print("{:d}".format(matrix[i][j] + 1))
-        except IndexError:
-            print()
+    for i in range(len(matrix)):
+        for j in range(0, len(matrix[i])):
+            if j < len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=' ')
+            else:
+                print("{:d}".format(matrix[i][j]), end='')
+        print()
