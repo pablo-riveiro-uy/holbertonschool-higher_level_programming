83a75f77b67d4a3928edc10b8970fd445e3a02ec
Pablo <priveiro@gmail.com>
2023-06-12 09:22:08 -0700

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,4 +12,8 @@ class Square:
         if int(__size) < 0:
             raise TypeError('size must be >= 0')
         self.__size = __size
+
+    def area(self):
+        """ Calc area of a sqaare of given size"""
+        return pow(self.__size, 2)
     pass
