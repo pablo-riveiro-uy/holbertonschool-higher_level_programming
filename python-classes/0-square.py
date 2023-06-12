e5f4c2bf0a7b2dda38ee16cba45970b946f3c7ce
Pablo <priveiro@gmail.com>
2023-06-12 08:14:33 -0700

    task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Contains a class for a Sqare """
+
+
+class Square:
+    """ Defines a Sqare, in facto no"""
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,3 @@
+	# Python - Classes and Objects
+	## Author
+	Pablo Riveiro
