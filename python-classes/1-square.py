8cadfa0153fe725f3808476d6696bec0ad072bb7
Pablo <priveiro@gmail.com>
2023-06-12 08:39:55 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Contains a class for a Sqare """
+
+
+class Square:
+    """ Defines a Sqare, still not"""
+
+    def __init__(self, __size):
+        self.__size = __size
+        """ Asigning a private atribute to self __size"""
+
+    pass
