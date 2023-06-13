ae88f17c242ebc81d5266272dadb89055192040a
root <priveiro@gmail.com>
2023-06-13 09:34:27 -0700

    task 0 b

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
+__import__("./test/add_integer.txt")
 def add_integer(a, b=98):
-    """
-    >>>(add_integer(1, 2)
-    3
-    >>>add_integer(100, -2)
-    98
-    >>>add_integer(100.3, -2)
-    100
-    >>>add_integer(2)
-    98
-    >>>add_integer(4, "School")
-    b must be an integer
-    >>>add_integer(None))
-    a must be an integer
-    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
     if not isinstance(b, int) and not isinstance(b, float):
