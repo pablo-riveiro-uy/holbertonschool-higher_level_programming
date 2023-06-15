e7efced8b022658b34ddec8069a14293b07b7f20
root <priveiro@gmail.com>
2023-06-15 10:13:59 -0700

    more test on 6

--- python-test_driven_development/6-main.py
+++ python-test_driven_development/6-main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""_summary_
+"""
 max_integer = __import__('6-max_integer').max_integer
 
 print(max_integer([1, 2, 3, 4]))
--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,3 +10,10 @@ class TestMaxInteger(unittest.TestCase):
         self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
     def test_value(self):
         self.assertRaises(TypeError, max_integer, "hola")
+    def test_negatives(self):
+        self.assertAlmostEqual(max_integer([-15, -7, -2, -9]), -2)
+    def test_one_only_value(self):
+        self.assertAlmostEqual(max_integer([7]), 7)
+    def test_empty_list(self):
+        self.assertAlmostEqual(max_integer([]), None)
+    
