a98140414ef04e531cf45dbfa468d5219a2b0de7
root <priveiro@gmail.com>
2023-06-15 10:24:00 -0700

    formatting files

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,13 +7,16 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
+
     def test_value(self):
         self.assertRaises(TypeError, max_integer, "hola")
+
     def test_negatives(self):
         self.assertAlmostEqual(max_integer([-15, -7, -2, -9]), -2)
+
     def test_one_only_value(self):
         self.assertAlmostEqual(max_integer([7]), 7)
+        
     def test_empty_list(self):
         self.assertAlmostEqual(max_integer([]), None)
     
