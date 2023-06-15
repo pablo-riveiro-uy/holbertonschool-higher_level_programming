2627745c6ed2c5f25fd7d28380cfadd6c6c81dda
root <priveiro@gmail.com>
2023-06-15 09:48:05 -0700

    task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
 import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-import ../6-max_integer
-
-class MyTests(unittest.TestCase):
-    def testIsList(self):
-        self.assertIsInstance(self.list, list)
+class TestMaxInteger(unittest.TestCase):
+    def test_max(self):
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
+    def test_value(self):
+        self.assertRaises(TypeError, max_integer, "hola")
