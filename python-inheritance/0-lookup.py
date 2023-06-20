d896a6d5b5bffd47e31e0b4b96eb6f904d0b8aaa
root <priveiro@gmail.com>
2023-06-20 10:46:46 -0700

    docs

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""_summary_
+    """
 def lookup(obj):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""_summary_
+    """
 class MyList(list):
+    """_summary_
+
+    Args:
+        list (_type_): _description_
+    """
     def print_sorted(self):
+        """_summary_
+        """
         print(sorted(self))
