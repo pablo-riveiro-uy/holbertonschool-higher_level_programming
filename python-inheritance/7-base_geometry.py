98a8689236f59f915bc53effcedef8ca724fa869
root <priveiro@gmail.com>
2023-06-20 13:26:17 -0700

    dale bo

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
 """_summary_
-
-    Raises:
-        Exception: _description_
 """
 
 
 class BaseGeometry():
     """_summary_
     """
+
     def area(self):
         """_summary_
-
-        Raises:
-            Exception: _description_
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """_summary_
-
-        Args:
-            name (_type_): _description_
-            value (_type_): _description_
-
-        Raises:
-            TypeError: _description_
-            ValueError: _description_
         """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
