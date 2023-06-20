20a842e2a00c20d12e35b6b121670b499584dd02
root <priveiro@gmail.com>
2023-06-20 11:55:59 -0700

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""_summary_
+    """
+
+
+def inherits_from(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if isinstance(obj, a_class) and not type(obj) == a_class:
+        return True
+    else:
+        return False
