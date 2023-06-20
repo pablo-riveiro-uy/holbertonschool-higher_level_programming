7ee1858c1dce323e6798a4eb3c24621969b06e9e
root <priveiro@gmail.com>
2023-06-20 11:26:07 -0700

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""_summary_
+    """
+
+
+def is_same_class(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
