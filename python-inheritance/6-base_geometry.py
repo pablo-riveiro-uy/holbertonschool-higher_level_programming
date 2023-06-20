8d098082236ad5241ecd7bb7e532350b48444b94
root <priveiro@gmail.com>
2023-06-20 12:13:47 -0700

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""_summary_
+
+    Raises:
+        Exception: _description_
+    """
+
+
+class BaseGeometry():
+    """_summary_
+    """
+    def area(self):
+        """_summary_
+
+        Raises:
+            Exception: _description_
+        """
+        raise Exception('area() is not implemented')
