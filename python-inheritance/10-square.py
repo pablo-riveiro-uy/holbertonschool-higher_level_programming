1824c393821081e4789233ae28c44ee4604e40c2
root <priveiro@gmail.com>
2023-06-20 14:35:11 -0700

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """_summary_
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Rentangle] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        return self.__size**2
