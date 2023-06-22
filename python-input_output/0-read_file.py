c0202f357d0b2c35c0c1c040bf59bce69e23495d
TheWhiteMachine <priveiro@gmail.com>
2023-06-22 09:16:31 -0700

    adding stringdoc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""_summary_
+    """
+
+
 def read_file(filename=""):
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read())
+
+
+"""_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+    """
