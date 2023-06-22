02758f96b10db8023b7814c6d11f8a63d6cf5dfa
TheWhiteMachine <priveiro@gmail.com>
2023-06-22 10:23:11 -0700

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""_summary_
+
+    Returns:
+        _type_: _description_"""
+import json
+
+
+def from_json_string(my_str):
+    """_summary_
+
+    Args:
+        my_str (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return json.loads(my_str)
