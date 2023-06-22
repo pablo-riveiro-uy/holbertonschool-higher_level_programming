4650567cdc2c325a214ae2fba8725b0e04f98086
TheWhiteMachine <priveiro@gmail.com>
2023-06-22 12:30:04 -0700

    task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+
+def class_to_json(obj):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return obj.__dict__
