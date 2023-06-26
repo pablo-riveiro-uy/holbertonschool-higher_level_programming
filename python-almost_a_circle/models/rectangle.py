6dd5bb7aeb7bf1df7b0ddad35f2ae6c8244c4f46
root <priveiro@gmail.com>
2023-06-26 07:21:47 -0700

    task 1

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""_summary_
+    """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """_summary_
+
+        Args:
+            width (_type_): _description_
+            height (_type_): _description_
+            x (int, optional): _description_. Defaults to 0.
+            y (int, optional): _description_. Defaults to 0.
+            id (_type_, optional): _description_. Defaults to None.
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__hight = height
+        self.__x = x
+        self.__y = y
