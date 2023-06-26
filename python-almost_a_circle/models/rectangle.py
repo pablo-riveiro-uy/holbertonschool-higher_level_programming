4d60343dc9a40401fc69cfe046780db61aae3fd4
root <priveiro@gmail.com>
2023-06-26 08:35:08 -0700

    tak 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,7 +22,35 @@ class Rectangle(Base):
             id (_type_, optional): _description_. Defaults to None.
         """
         super().__init__(id)
-        self.__width = width
-        self.__hight = height
-        self.__x = x
-        self.__y = y
+
+        @property
+        def width(self):
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            self.__width = value
+
+        @property
+        def height(self):
+            return self.__height
+
+        @height.setter
+        def width(self, value):
+            self.__height = value
+
+        @property
+        def x(self):
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            self.__x = x
+
+        @property
+        def y(self):
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            self.__y = y
