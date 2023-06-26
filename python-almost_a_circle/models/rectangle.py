43c399bfef0c50a1a285a2b8b0a39d3345ec8efa
root <priveiro@gmail.com>
2023-06-26 12:33:29 -0700

    task 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,17 @@ class Rectangle(Base):
         """
         txt = "[Rectangle] ({}) {}/{} - {}/{}"
         return txt.format(self.id, self.x, self.y, self.__width, self.__height)
+
+    def update(self, *args):
+        data = ["id", "width", "height", 'x', 'y']
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
