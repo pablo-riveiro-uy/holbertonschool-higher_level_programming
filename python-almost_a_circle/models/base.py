532641bc7130c2e8399f57314b98de3e7c657ea6
root <priveiro@gmail.com>
2023-06-26 06:49:49 -0700

    task 0

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+class Base:
+    """_summary_
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+
+        """_summary_
+
+        Args:
+            id (_type_, optional): _description_. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
