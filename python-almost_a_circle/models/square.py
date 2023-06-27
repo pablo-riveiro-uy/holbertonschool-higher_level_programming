7dda69a50749cce99b8fac9172e0d3a971d89810
TheWhiteMachine <priveiro@gmail.com>
2023-06-27 09:57:56 -0700

    task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,6 +14,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
 
+    @property
+    def size(self):
+        return self.size
+
+    @size.setter
+    def size(self, value):
+        self.update(width=value, height=value)
+
     def __str__(self):
         """_summary_
 
