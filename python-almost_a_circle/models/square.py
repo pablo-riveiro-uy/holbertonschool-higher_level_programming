3a773437c5cf37726a4eaae30bf26427a7846b87
TheWhiteMachine <priveiro@gmail.com>
2023-06-27 09:45:18 -0700

    task 2 str fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,11 +14,11 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
 
-        def __str__(self):
-            """_summary_
+    def __str__(self):
+        """_summary_
 
-            Returns:
-                _type_: _description_
-            """
-            txt = "[Square] ({}) {}/{} - {}"
-            return txt.format(self.id, self.x, self.y, self.size)
+        Returns:
+            _type_: _description_
+        """
+        txt = "[Square] ({}) {}/{} - {}"
+        return txt.format(self.id, self.x, self.y, self.height)
