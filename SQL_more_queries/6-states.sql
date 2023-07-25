6e75bd2945a16df35dcb7aec4f9caa60e560c4e8
root <priveiro@gmail.com>
2023-07-25 10:18:53 -0700

    task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- States in table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT UNIQUE NOT NULL PRIMARY KEY  AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL);
