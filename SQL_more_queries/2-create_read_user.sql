5225f16af1d474e701df80da4d1d6d28a5e7c58e
root <priveiro@gmail.com>
2023-07-25 11:08:49 -0700

    fixing 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,6 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_2_pwd';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
 
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
