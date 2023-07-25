f3e011605ff27ded6e3c27f1895d5bb539879c3f
root <priveiro@gmail.com>
2023-07-25 08:04:43 -0700

    task 1 c

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,10 @@
 -- Creating Users
 
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+
 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
