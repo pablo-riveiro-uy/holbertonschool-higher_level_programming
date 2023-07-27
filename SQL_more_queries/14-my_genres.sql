5bfa9ab5bdc7116bc5e1d8bdd19e5b8ec1324e20
root <priveiro@gmail.com>
2023-07-27 09:41:10 -0700

    fix 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- Generes of a show by name
 
-SELECT name FROM tv_genres
-INNER JOIN tv_show INNER JOIN tv_show_genres on tv_genres.id = tv_show.genre_id
-WHERE tv_genres.title = 'Comedy'
-ORDER BY name ASC;
+SELECT name FROM tv_genres, tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = show_id
+WHERE title = "Dexter" AND genre_id = tv_genres.id
+ORDER BY name;
