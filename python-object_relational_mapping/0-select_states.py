40c9ad2237ec5288538e91acdb933421b37efbad
root <priveiro@gmail.com>
2023-07-28 06:23:24 -0700

    formatting doc

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,12 @@ from sys import argv
 if __name__ == '__main__':
     """ make a conection and print the states """
 
-    db=MySQLdb.connect("localhost", 
-                  argv[1], argv[2], argv[3])
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
 
-
-    info=db.cursor()
+    info = db.cursor()
     info.execute("SELECT * FROM states ORDER BY states.id ASC ")
 
     for states in info.fetchall():
         print(states)
 
     db.close()
-
-
