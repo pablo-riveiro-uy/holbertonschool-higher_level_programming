37340dc470b8a7ce17db024620a527bb5a5a9427
TheWhiteMachine <priveiro@gmail.com>
2023-07-29 12:41:04 -0700

    task 13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" delete a State that conmtains a  """
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy import delete
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query_all = session.query(State).all()
+
+    for row in query_all:
+        if 'a' in row.name:
+            session.query(State).filter(State.id == row.id).delete()
+    
+    session.commit()
